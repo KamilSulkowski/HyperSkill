ee5ff13bbba37cc99a0b5a03d64561206dd099b5
KamilSulkowski <97765669+KamilSulkowski@users.noreply.github.com>
2022-03-22 23:21:07 +0100

    Update Cinema.java

--- Cinema.java
+++ Cinema.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
 public class Cinema {
     static Scanner sc = new Scanner(System.in);
     static boolean exit = true;
+    static int tickets = 0;
+    static double percent;
+    static boolean bought = false;
+    static int totalPrice;
+    static int income = 0;
     static public int n;
     static public int m;
     static public int x;
@@ -19,6 +24,7 @@ public class Cinema {
             System.out.println();
             System.out.println("1. Show the seats");
             System.out.println("2. Buy a ticket");
+            System.out.println("3. Statistics");
             System.out.println("0. Exit");
             int option = sc.nextInt();
             switch (option) {
@@ -31,37 +37,23 @@ public class Cinema {
                 case 2:
                     buyTicket();
                     break;
+                case 3:
+                    stats();
+                    break;
+                case 4:
+
+                    break;
             }
         }
     }
-    public static void sold() {
+    public static void stats() {
         amount = n * m;
-        int halfN = 0;
-        int halfPrice = 0;
-        int nextHalfPrice = 0;
-        int totalPrice = 0;
-        int price = 0;
-        if (amount > 60) {
-            if (n % 2 == 0){
-                halfN  = n/2;
-            }
-            price = 10;
-            halfPrice = (halfN * m) * price;
-            if (n % 2 != 0) {
-                ++halfN;
-            }
-            price = 8;
-            nextHalfPrice = (halfN * m) * price;
-            totalPrice = nextHalfPrice + halfPrice;
+        totalPrice = 0;
+        System.out.println("Number of purchased tickets: " + Cinema.tickets);
+        System.out.println("Percentage: " + String.format("%.2f",Cinema.percent) + "%");
+        System.out.println("Current income: $" + Cinema.income);
+        System.out.println("Total income: $" + totalIncome());
 
-        }else {
-            if (amount < 60){
-                price = 10;
-            }
-            totalPrice = amount * price;
-        }
-        System.out.println("Total income: ");
-        System.out.println("$"+totalPrice);
     }
     public static void menuCinema() {
         System.out.println("Enter the number of rows: ");
@@ -70,17 +62,30 @@ public class Cinema {
         m = sc.nextInt();
     }
     public static void buyTicket() {
-
         System.out.println();
         System.out.println("Enter a row number: ");
         x = sc.nextInt();
         System.out.println("Enter a seat number in that row: ");
         y = sc.nextInt();
-        array[x-1][y-1] = 'B';
+        try {
+            if (array[x-1][y-1] == 'B') {
+                System.out.println();
+                System.out.println("That ticket has already been purchased!");
+            } else {
+                Cinema.bought = true;
+                purchasedTickets();
+                incomeFromTicket();
+                Cinema.percent = (float) Cinema.tickets / amount;
+                Cinema.percent *= 100;
+                array[x-1][y-1] = 'B';
+                Cinema.bought = false;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Wrong input!");
+        }
         System.out.println();
         System.out.println("Ticket price: $" + ticketPrice());
         System.out.println();
-        readRoom();
     }
     static public void readRoom() {
         System.out.println();
@@ -113,8 +118,61 @@ public class Cinema {
         if (amount > 60) {
             if (x <= 4) {
                 ticketP = 10;
-            }else ticketP = 8;
+            }else {
+                ticketP = 8;
+            }
         }
         return ticketP;
     }
+    static public void incomeFromTicket() {
+        int ticketP;
+        amount = n * m;
+        if (amount < 60){
+            ticketP = 10;
+            Cinema.income = Cinema.income + ticketP;
+        }
+        if (amount > 60) {
+            if (x <= 4) {
+                ticketP = 10;
+            }else {
+                ticketP = 8;
+            }
+            Cinema.income = Cinema.income + ticketP;
+        }
+    }
+    static public void purchasedTickets() {
+        if (bought) {
+            Cinema.tickets++;
+        }
+    }
+    static public int totalIncome() {
+        int halfN = 0;
+        int halfPrice;
+        int nextHalfPrice;
+        int ticketP;
+        totalPrice = 0;
+        amount = n * m;
+        if (amount < 60){
+            ticketP = 10;
+            totalPrice = amount * ticketP;
+        }
+        if (amount > 60) {
+            if (n % 2 == 0){
+                halfN  = n/2;
+            }
+            if (n % 2 != 0) {
+                halfN  = (n/2);
+            }
+            ticketP = 10;
+            halfPrice = (halfN * m) * ticketP;
+            if (n % 2 != 0) {
+                ++halfN;
+            }
+            ticketP = 8;
+            nextHalfPrice = (halfN * m) * ticketP;
+            totalPrice = nextHalfPrice + halfPrice;
+        }
+        return totalPrice;
+    }
+
 }
