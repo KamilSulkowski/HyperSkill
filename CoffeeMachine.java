bb5bcf2f95196b7685cde288e741de899f5089b7
KamilSulkowski <97765669+KamilSulkowski@users.noreply.github.com>
2022-03-23 13:36:17 +0100

    v2
    Stage 2/6: Ingredient calculator
    Description
    Now let's consider a case where you need a lot of coffee. Maybe, for example, you’re hosting a party with a lot of guests. The program should calculate how much water, coffee, and milk are necessary to make the specified amount of coffee. One cup of coffee made on this coffee machine contains 200 ml of water, 50 ml of milk, and 15 g of coffee beans.
    
    The user should input the amount of coffee he needs, in cups, for all the guests.
    
    Of course, all this coffee is not needed right now, so at this stage, the coffee machine doesn’t actually make any coffee.
    
    Instruction
    Write a program that calculates the amount of ingredients needed to make a certain amount of coffee.
    
    Examples
    The example below shows how your output might look.
    The symbol > represents the user input. Note that it's not part of the input.
    
    Example 1
    
    Write how many cups of coffee you will need: 
    > 25
    For 25 cups of coffee you will need:
    5000 ml of water
    1250 ml of milk
    375 g of coffee beans

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,13 +1,32 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
+    static int CoC;
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
+
+        System.out.println("Write how many cups of coffee you will need:");
+        CoffeeMachine.CoC = sc.nextInt();
+        calc();
+        /*System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
         System.out.println("Mixing boiled water with crushed coffee beans");
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Coffee is ready!");*/
     }
+    static public void calc() {
+        int cWater = 200;
+        int cMilk = 50;
+        int cCoffeeB = 15;
+        System.out.println("For " + CoffeeMachine.CoC + " cups of coffee you will need:");
+        System.out.println(CoffeeMachine.CoC * cWater + " ml of water");
+        System.out.println(CoffeeMachine.CoC * cMilk + " ml of milk");
+        System.out.println(CoffeeMachine.CoC * cCoffeeB + " g of coffee beans");
+
+    }
+
 }
