6083839145c0e2a55dcb60f21750768ed1001f0d
KamilSulkowski <97765669+KamilSulkowski@users.noreply.github.com>
2022-03-24 14:50:33 +0100

    v3
    Stage 3/6: Estimate the number of servings
    Description
    A real coffee machine never has an infinite supply of water, milk, or coffee beans. And if you input a really big number, it’s almost certain that a real coffee machine wouldn't have the supplies needed to make all that coffee.
    
    In this stage, you need to improve the previous program. Now you need to input amounts of water, milk, and coffee beans that your coffee machine has at the moment.
    
    If the coffee machine has enough supplies to make the specified amount of coffee, the program should print "Yes, I can make that amount of coffee". If the coffee machine can make more than that, the program should output "Yes, I can make that amount of coffee (and even N more than that)", where N is the number of additional cups of coffee that the coffee machine can make. If the amount of resources is not enough to make the specified amount of coffee, the program should output "No, I can make only N cup(s) of coffee".
    
    Like in the previous stage, the coffee machine needs 200 ml of water, 50 ml of milk, and 15 g of coffee beans to make one cup of coffee.
    
    Instruction
    Write a program that calculates whether it will be able to make the required amount of coffee from the specified amount of ingredients.
    
    Examples
    The program should firstly request for water, then milk, then beans, and then the number of cups.
    The symbol > represents the user input. Note that it's not part of the input.
    
    Example 1
    
    Write how many ml of water the coffee machine has: 
    > 300
    Write how many ml of milk the coffee machine has: 
    > 65
    Write how many grams of coffee beans the coffee machine has: 
    > 100
    Write how many cups of coffee you will need: 
    > 1
    Yes, I can make that amount of coffee
    Example 2
    
    Write how many ml of water the coffee machine has: 
    > 500
    Write how many ml of milk the coffee machine has: 
    > 250
    Write how many grams of coffee beans the coffee machine has: 
    > 200
    Write how many cups of coffee you will need: 
    > 10
    No, I can make only 2 cup(s) of coffee
    Example 3
    
    Write how many ml of water the coffee machine has: 
    > 1550
    Write how many ml of milk the coffee machine has: 
    > 299
    Write how many grams of coffee beans the coffee machine has: 
    > 300
    Write how many cups of coffee you will need: 
    > 3
    Yes, I can make that amount of coffee (and even 2 more than that)
    Example 4
    
    Write how many ml of water the coffee machine has: 
    > 0
    Write how many ml of milk the coffee machine has: 
    > 0
    Write how many grams of coffee beans the coffee machine has: 
    > 0
    Write how many cups of coffee you will need: 
    > 1
    No, I can make only 0 cup(s) of coffee
    Example 5
    
    Write how many ml of water the coffee machine has: 
    > 0
    Write how many ml of milk the coffee machine has: 
    > 0
    Write how many grams of coffee beans the coffee machine has: 
    > 0
    Write how many cups of coffee you will need: 
    > 0
    Yes, I can make that amount of coffee 
    Example 6
    
    Write how many ml of water the coffee machine has: 
    > 200
    Write how many ml of milk the coffee machine has: 
    > 50
    Write how many grams of coffee beans the coffee machine has: 
    > 15
    Write how many cups of coffee you will need: 
    > 0
    Yes, I can make that amount of coffee (and even 1 more than that)

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     static int CoC;
+    static int cWater = 200;
+    static int cMilk = 50;
+    static int cCoffeeB = 15;
+    static int iWater = 0;
+    static int iMilk = 0;
+    static int iCoffeeB = 0;
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
-        System.out.println("Write how many cups of coffee you will need:");
-        CoffeeMachine.CoC = sc.nextInt();
-        calc();
+        storageMachine();
+        coffeeMake();
+        //calc();
         /*System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
@@ -18,15 +24,106 @@ public class CoffeeMachine {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");*/
     }
-    static public void calc() {
-        int cWater = 200;
-        int cMilk = 50;
-        int cCoffeeB = 15;
+   /* static public void calc() {
+        System.out.println("Write how many cups of coffee you will need:");
+        CoffeeMachine.CoC = sc.nextInt();
         System.out.println("For " + CoffeeMachine.CoC + " cups of coffee you will need:");
-        System.out.println(CoffeeMachine.CoC * cWater + " ml of water");
-        System.out.println(CoffeeMachine.CoC * cMilk + " ml of milk");
-        System.out.println(CoffeeMachine.CoC * cCoffeeB + " g of coffee beans");
+        System.out.println(CoffeeMachine.CoC * CoffeeMachine.cWater + " ml of water");
+        System.out.println(CoffeeMachine.CoC * CoffeeMachine.cMilk + " ml of milk");
+        System.out.println(CoffeeMachine.CoC * CoffeeMachine.cCoffeeB + " g of coffee beans");
+
+    }*/
+
+    static public void storageMachine() {
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        CoffeeMachine.iWater = sc.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        CoffeeMachine.iMilk = sc.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        CoffeeMachine.iCoffeeB = sc.nextInt();
+    }
+    static public void coffeeMake() {
+        System.out.println("Write how many cups of coffee you will need: ");
+        CoffeeMachine.CoC = sc.nextInt();
+        boolean space = true;
+        int waterForCoC = CoffeeMachine.CoC * CoffeeMachine.cWater;
+        int milkForCoC = CoffeeMachine.CoC * CoffeeMachine.cMilk;
+        int cBeansForCoC = CoffeeMachine.CoC * CoffeeMachine.cCoffeeB;
+        int moreWater = CoffeeMachine.iWater - waterForCoC;
+        int moreMilk = CoffeeMachine.iMilk - milkForCoC;
+        int moreCoffeeB = CoffeeMachine.iCoffeeB  - cBeansForCoC;
+        int moreCoC = 0;
+        if (CoffeeMachine.CoC >= 0) {
+            if (CoffeeMachine.iWater <= waterForCoC || CoffeeMachine.iMilk <= milkForCoC || CoffeeMachine.iCoffeeB <= cBeansForCoC) {
+                while (space) {
+                    if (CoffeeMachine.iWater > CoffeeMachine.cWater) {
+                        CoffeeMachine.iWater -= CoffeeMachine.cWater;
+                        if (CoffeeMachine.iMilk > CoffeeMachine.cMilk) {
+                            CoffeeMachine.iMilk -= CoffeeMachine.cMilk;
+                            if (CoffeeMachine.iCoffeeB > CoffeeMachine.cCoffeeB) {
+                                moreCoC += 1;
+                                CoffeeMachine.iCoffeeB -= CoffeeMachine.cCoffeeB;
+                            } else {
+                                space = false;
+                            }
+                        } else {
+                            space = false;
+                        }
+                    } else {
+                        space = false;
+                    }
+                }
+                System.out.println("No, I can make only " + moreCoC + " cup(s) of coffee");
+                }
+
+            if (CoffeeMachine.iWater >= waterForCoC) {
+                if (CoffeeMachine.iMilk >= milkForCoC) {
+                    if (CoffeeMachine.iCoffeeB >= cBeansForCoC) {
+                        while (space) {
+                            if (moreWater >= CoffeeMachine.cWater) {
+                                moreWater -= CoffeeMachine.cWater;
+                                if (moreMilk >= CoffeeMachine.cMilk) {
+                                    moreMilk -= CoffeeMachine.cMilk;
+                                    if (moreCoffeeB >= CoffeeMachine.cCoffeeB) {
+                                        moreCoC += 1;
+                                        moreCoffeeB -= CoffeeMachine.cCoffeeB;
+                                    } else {
+                                        space = false;
+                                    }
+                                } else {
+                                    space = false;
+                                }
+                            } else {
+                                space = false;
+                            }
+                        }
+                        if (moreCoC == 0) {
+                            if (CoffeeMachine.CoC == 0) {
+                                System.out.println("Yes, I can make that amount of coffee");
+                            }else if (CoffeeMachine.CoC == 1) {
+                                System.out.println("Yes, I can make that amount of coffee");
+                            }else if (CoffeeMachine.CoC > 1) {
+                                System.out.println("Yes, I can make that amount of coffee");
+                            }else {
+                                System.out.println("No, I can make only " + moreCoC + " cup(s) of coffee");
+                            }
+                        } else {
+                            if (CoffeeMachine.CoC == 0) {
+                                System.out.println("Yes, I can make that amount of coffee (and even " + moreCoC + " more than that)");
+                            }else if (CoffeeMachine.CoC == 1) {
+                                System.out.println("Yes, I can make that amount of coffee(and even " + moreCoC + " more than that)");
+                            }else if (CoffeeMachine.CoC > 1) {
+                                System.out.println("Yes, I can make that amount of coffee (and even " + moreCoC + " more than that)");
+                            }else {
+                                System.out.println("No, I can make only " + moreCoC + " cup(s) of coffee");
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
 
     }
-
 }
